824e3c9899fd59b8076295dd9b7f221fe147a685
ajohns038 <andrewjohnson038@gmail.com>
2024-04-21 18:51:36 -0500

    commit 3:
    In the "Artists" tab on the excel sheet (Spotify2023_artists df) The following updates were made: De-dupped artist and summed total streams by artist; added rank column to the data frame to rank artists by streams. This will allow rank to act as a card visual in Tableau. Reduced the rows to the top 100 artists for simplicity purposes.
    
    commit 3.1: filled in null values in the 'key' column

--- Spotify_Proj/SpotifyAnalysis.py
+++ Spotify_Proj/SpotifyAnalysis.py
@@ -8,7 +8,7 @@
 # pip install zipfile
 # pip install kaggle
 
-# import pandas library
+# import pandas library (used to create a data frame and wrangle data)
 import pandas as pd
 # import zipfile library (used to extract the file from kaggle)
 import zipfile
@@ -28,35 +28,35 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 api = KaggleApi()
 api.authenticate()
 
-# download dataset with kaggle (API GitHub directions on how to use https://github.com/Kaggle/kaggle-api)
+# Download dataset with kaggle (API GitHub directions on how to use https://github.com/Kaggle/kaggle-api)
 dataset = 'nelgiriyewithana/top-spotify-songs-2023'
 kaggle.api.dataset_download_files(dataset, path='/Users/andrewjohnson/IdeaProjects/DataSciencePortfolio/Spotify_Proj')
 
-# unzip the file from the download zip file ['r' leaves the backslashes in the string]
+# Unzip the file from the download zip file ['r' leaves the backslashes in the string]
 zipfile_name = '/Users/andrewjohnson/IdeaProjects/DataSciencePortfolio/Spotify_Proj/top-spotify-songs-2023.zip'
 with zipfile.ZipFile(zipfile_name, 'r') as file:
     file.extractall()
 
-# set path name for csvfile
+# Set path name for csvfile
 path = "/Users/andrewjohnson/IdeaProjects/DataSciencePortfolio/Spotify_Proj/spotify-2023.csv"
 
-# read the csv file as a pandas df
-# the file being used is not encoded in UTF-8, which is needed to have the csv be encoded to UTF-8. It is encoded in ISO-8859-1, so we will need to change that
+# Read the csv file as a pandas df
+# The file being used is not encoded in UTF-8, which is needed to have the csv be encoded to UTF-8. It is encoded in ISO-8859-1, so we will need to change that
 Spotify2023 = pd.read_csv(path, encoding='ISO-8859-1') #csv is downloaded in ISO-8859-1 format
 Spotify2023.to_csv(path, encoding='utf-8', index=False)
 
 
-# get basic summary of the data
+# Get basic summary of the data
 Spotify2023.info()
 
-# get a count of each unique value in the song key column
+# Get a count of each unique value in the song key column
 print(Spotify2023.key.value_counts())
 
-# change the column name of "artist(s)_name column to artist_name(s)"
+# Change the column name of "artist(s)_name column to artist_name(s)"
 Spotify2023 = Spotify2023.rename(columns={'artist(s)_name':'artist_name(s)'})
 Spotify2023.info()
 
-# create a dictionary to convert month IDs to month name
+# Create a dictionary to convert month IDs to month name
 months_dictionary = {
     '1' : 'January',
     '2' : 'February',
@@ -70,20 +70,50 @@ months_dictionary = {
     '10': 'October',
     '11' : 'November',
     '12' : 'December'}
-# change values to a string
+# Change values to a string
 Spotify2023.released_month = Spotify2023.released_month.astype('str')
-# map the month IDs to dictionary containing the month names
+# Map the month IDs to dictionary containing the month names
 Spotify2023.released_month = Spotify2023.released_month.map(months_dictionary)
 
-# view final dataset to check data cleansing done
-# print(Spotify2023.to_string()) #view whole output
+# View final dataset to check data cleansing done
+# Print(Spotify2023.to_string()) #view whole output
 print(Spotify2023.head(50).to_string()) #limit to only viewing 50 rows
 
-# write the final df to an excel sheet for visualization
+# When checking the dataset, I noticed there are some null values in the "key" column. Let's fill these as we will be doing analysis on most popular keys in our visual.
+# For our visual, we will only be using songs released in 2023; let's reduce the sample size to songs released in 2023
+Tracks_2023 = Spotify2023[Spotify2023['released_year'] == 2023]
+# Let's now isolate the null values in the "key" column to see which songs we will need to fill nulls
+Key_Nulls_2023 = Tracks_2023['key'].isnull()
+print(Tracks_2023[Key_Nulls_2023].to_string())
+# Print the count of rows to see count of tracks with a null key value
+print("Number of Track w/ a Null Value:", len(Tracks_2023[Key_Nulls_2023])) #len function counts the values
+# There are only 16 tracks released in 2023 that have a null key value. Considering the # of nulls is so low, the easiest method would be to fill the nulls manually. Let's do that in this next step:
+# Fill null values manually in the 'key' column for specified rows with different values
+Spotify2023.loc[12, 'key'] = 'Am'
+Spotify2023.loc[17, 'key'] = 'C#'
+Spotify2023.loc[35, 'key'] = 'C#'
+Spotify2023.loc[44, 'key'] = 'C#'
+Spotify2023.loc[58, 'key'] = 'Cm'
+Spotify2023.loc[135, 'key'] = 'C#'
+Spotify2023.loc[144, 'key'] = 'C#'
+Spotify2023.loc[151, 'key'] = 'C#'
+Spotify2023.loc[181, 'key'] = 'C#'
+Spotify2023.loc[259, 'key'] = 'C#'
+Spotify2023.loc[287, 'key'] = 'Cm'
+Spotify2023.loc[332, 'key'] = 'C#'
+Spotify2023.loc[373, 'key'] = 'C#'
+Spotify2023.loc[379, 'key'] = 'C#'
+Spotify2023.loc[381, 'key'] = 'C#'
+Spotify2023.loc[385, 'key'] = 'C#'
+# Let's check the nulls have now been filled by checking the song "flowers" was filled with 'Am' (row 12) and re-printing null count
+print(Spotify2023.at[12, 'key']) # checking song key was filled
+# Success: We can now write the df to excel for visualization
+
+# Write the final df to an excel sheet for visualization
 Spotify2023.to_excel('Spotify2023_TableauProj.xlsx', sheet_name='Spotify2023_stats')
 
-# for the artists tab in Tableau, I want to create a new tab in the excel sheet (new table) where songs with multiple artists are split into their own rows so that we can rank individual artists by streams
-# to do this, we will need to split the delimiter (delimted by a comma) and add a new tab to the excel sheet for our new table
+# For the artists tab in Tableau, I want to create a new tab in the excel sheet (new table) where songs with multiple artists are split into their own rows so that we can rank individual artists by streams
+# To do this, we will need to split the delimiter (delimted by a comma) and add a new tab to the excel sheet for our new table
 
 # Step 1: Split the artists column based on comma delimiter
 Spotify2023['artist_name(s)'] = Spotify2023['artist_name(s)'].str.split(', ')
@@ -91,7 +121,7 @@ Spotify2023['artist_name(s)'] = Spotify2023['artist_name(s)'].str.split(', ')
 # Step 2: Explode the list of artists
 Spotify2023_artists = Spotify2023.explode('artist_name(s)')
 
-#Step 2.1: lets drop the fields we don't need. Let's drop all fields except artist name and streams.
+# Step 2.1: lets drop the fields we don't need. Let's drop all fields except artist name and streams.
 Spotify2023_artists = Spotify2023_artists.drop(columns=[
     'artist_count',
     'track_name',
@@ -119,8 +149,8 @@ Spotify2023_artists = Spotify2023_artists.drop(columns=[
 
 # Step 3: Check that the new df delimited and columns dropped properly.
 print(Spotify2023_artists.head(50).to_string()) # This limits to viewing only 50 rows of the df
-# to view the whole output, use: print(Spotify2023_artists.to_string())
-# looks good. On to next step.
+# To view the whole output, use: print(Spotify2023_artists.to_string())
+# Looks good. On to next step.
 
 # Step 4: Deduplicate Artists & Sum the Streams
 # First, let's check that 'streams' was brought in as an integer and not an object:
